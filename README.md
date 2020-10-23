85ff7bb4a3ea07ee94f1fcadb2d25c1107765aa8
Георгиевский Максим Игоревич <georgievskiy.mi@defo.local>
2020-10-23 18:00:02 +0300

    Initial commit - added global styles and started first-screen

---
+++ README.md
@@ -0,0 +1,9 @@
+# project-template
+
+## How to use:
+
+* run 'gulp' for coding.
+* all styles concatinating in 'main.min.css'. add additional css files by copying them into css folder (or subfolders)
+* all additional *.scss must be imported into main.scss
+* all scripts contatinating into 'main.min.js'. add additional js files by putting them into js folder (or subfolders) 
+* run 'gulp build' to build project into 'dist' directory (only minified files)
---
+++ gulpfile.js
@@ -0,0 +1,118 @@
+// VARIABLES & PATHS
+
+let fileswatch   = 'html,htm,css,scss,js,txt,json,md,woff2', // List of files extensions for watching & hard reload (comma separated)
+	imageswatch  = 'jpg,jpeg,png,webp,svg', // List of images extensions for watching & compression (comma separated)
+	fonttypes	 = 'woff,woff2,ttf,eot'		
+	baseDir      = 'app', // Base directory path without «/» at the end
+	distDir		 = 'dist'	// Production directory
+    online       = true; // If «false» - Browsersync will work offline without internet connection
+
+let paths = {
+
+	scripts: {
+		src: [
+			baseDir + '/js/**/*.js',
+			'!' + baseDir + '/js/main.js',
+			'!' + baseDir + '/js/main.min.js',
+			baseDir + '/js/main.js', // main.js. Always at the end
+		],
+		dest: baseDir + '/js',
+		dist: distDir + '/js',
+	},
+
+	styles: {
+		src: [
+			baseDir + '/css/**/*.css',
+			'!' + baseDir + '/js/main.css',
+			'!' + baseDir + '/js/main.min.css',
+			baseDir + '/scss/main.scss' // main.css. Always at the end
+		],
+		dest: baseDir + '/css',
+		dist: distDir + '/css',
+	},
+
+	images: {
+		src:  baseDir + '/img-src/**/*',
+		dest: baseDir + '/img',
+		dist: distDir + '/img',
+	},
+
+	cssOutputName: 'main.min.css',
+	jsOutputName:  'main.min.js',
+
+}
+
+// LOGIC
+
+const { src, dest, parallel, series, watch } = require('gulp');
+const scss         = require('gulp-sass');
+const cleancss     = require('gulp-clean-css');
+const concat       = require('gulp-concat');
+const browserSync  = require('browser-sync').create();
+const uglify       = require('gulp-uglify-es').default;
+const autoprefixer = require('gulp-autoprefixer');
+const imagemin     = require('gulp-imagemin');
+const newer        = require('gulp-newer');
+const del          = require('del');
+
+function browsersync() {
+	browserSync.init({
+		server: { baseDir: baseDir + '/' },
+		notify: false,
+		online: online
+	})
+}
+
+function scripts() {
+	return src(paths.scripts.src)
+	.pipe(concat(paths.jsOutputName))
+	.pipe(uglify())
+	.pipe(dest(paths.scripts.dest))
+	.pipe(browserSync.stream())
+}
+
+function styles() {
+	return src(paths.styles.src)
+	.pipe(scss())
+	.pipe(concat(paths.cssOutputName))
+	.pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid: true }))
+	.pipe(cleancss( {level: { 1: { specialComments: 0 } },/* format: 'beautify' */ }))
+	.pipe(dest(paths.styles.dest))
+	.pipe(browserSync.stream())
+}
+
+function images() {
+	return src(paths.images.src)
+	.pipe(newer(paths.images.dest))
+	.pipe(imagemin())
+	.pipe(dest(paths.images.dest))
+}
+
+function cleanimg() {
+	return del('' + paths.images.dest + '/**/*', { force: true })
+}
+
+function startwatch() {
+	watch(baseDir  + '/**/scss/**/*', styles);
+	watch(baseDir  + '/**/*.{' + imageswatch + '}', images);
+	watch(baseDir  + '/**/*.{' + fileswatch + '}').on('change', browserSync.reload);
+	watch([baseDir + '/**/*.js', '!' + paths.scripts.dest + '/*.min.js'], scripts);
+}
+
+function build() {
+	return src([baseDir + '/css/main.min.css',
+				baseDir + '/js/main.min.js',
+				baseDir + '/img/*',
+				baseDir + '/fonts/*',
+				baseDir + '/**/*.{html,htm}'], { base: './app' })
+		   .pipe(dest(distDir))
+}
+
+exports.browsersync = browsersync;
+exports.assets      = series(cleanimg, styles, scripts, images);
+exports.styles      = styles;
+exports.scripts     = scripts;
+exports.images      = images;
+exports.cleanimg    = cleanimg;
+exports.default     = parallel(images, styles, scripts, browsersync, startwatch);
+exports.build 		= series(cleanimg, scripts, styles, images, build);
